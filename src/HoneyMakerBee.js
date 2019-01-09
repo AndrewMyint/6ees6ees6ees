7267c8aeadb3e7d8c8a5b095483d7f1dc1771a28
Andrew Myint <doku.potharhtoo@gmail.com>
2019-01-08 19:23:26 -0500

    Complete HoneyMakerBee"

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,15 @@
-class HoneyMakerBee {
+class HoneyMakerBee extends Bee{
   // TODO..
+  constructor(age, color, food, job) {
+    super();
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+  }
+  makeHoney() {
+    this.honeyPot++;
+  }
+  giveHoney() {
+    this.honeyPot--;
+  }
 };
